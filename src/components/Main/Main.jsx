a323a21cd0d5c8bec6d0e9e4e4d300ed36a2f17a
enemy111 <andrian.kobryn.knm.2019@lpnu.ua>
2022-12-22 13:11:24 +0200

    push

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,12 @@
 import styles from './Main.module.scss';
 import Select from 'react-select';
-import japan from '../../style/images/japan.jfif'
-import spain from '../../style/images/spain.jpg'
+import warszawa from '../../style/images/Warszawa.jpg'
 import video from '../../style/images/Swimming.mp4'
 import { Slider } from '../Carousel/Slider';
-import ital from '../../style/images/Italy.jpg'
-import ukraine from '../../style/images/ukraine.jpg'
+import kyiv from '../../style/images/Kyiv.jpeg'
+import tokyo from '../../style/images/Tokyo.jpg'
 import madrid from '../../style/images/madrid.jpg'
+import paris from '../../style/images/paris.jpg'
 import { Link, Outlet } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -40,35 +40,39 @@ const Main = props => {
                         <Link to='madrid'>Madrid</Link>
                     </div>
                     <div className={styles.mainBlocksInfoDescription}> Столиця та найбільше місто Іспанії, адміністративний центр автономної спільноти Мадрид. Розташоване в центральній частині Піренейського півострова.
-
-
-                        {/* <div>{props.selectedOption ? props.selectedOption.value : 'loh'}</div> */}
-
-
                         <Link to='madrid' className={styles.mainBlocksInfoButton}>Перейти</Link>
                     </div>
                 </div>
-                <div className={styles.mainBlocksInfo}><img src={ital} alt="" className={styles.imgs} />
+                <div className={styles.mainBlocksInfo}><img src={paris} alt="" className={styles.imgs} />
                     <div href='' className={styles.mainBlocksInfoTitle}>
-                        <Link to='login'>Italy</Link>
+                        <Link to='madrid'>Paris</Link>
                     </div>
-
-                </div>
-                <div className={styles.mainBlocksInfo}><img src={spain} alt="" className={styles.imgs} />
-                    <div href='' className={styles.mainBlocksInfoTitle}>
-                        <Link to='login'>Spain</Link>
+                    <div className={styles.mainBlocksInfoDescription}> Столиця Франції, адміністративний центр регіону Іль-де-Франс. Окремий департамент Франції. Розташований на річці Сена
+                        <Link to='madrid' className={styles.mainBlocksInfoButton}>Перейти</Link>
                     </div>
-
                 </div>
-                <div className={styles.mainBlocksInfo}><img src={ukraine} alt="" className={styles.imgs} />
+                <div className={styles.mainBlocksInfo}><img src={kyiv} alt="" className={styles.imgs} />
                     <div href='' className={styles.mainBlocksInfoTitle}>
-                        <Link to='login'>Ukraine</Link>
+                        <Link to='login'>Kyiv</Link>
                     </div>
-
+                    <div className={styles.mainBlocksInfoDescription}> столиця та найбільше місто України, одне з найбільших і найстаріших міст Європи. Розташований у середній течії Дніпра, у північній Наддніпрянщині.
+                        <Link to='madrid' className={styles.mainBlocksInfoButton}>Перейти</Link>
+                    </div>
                 </div>
-                <div className={styles.mainBlocksInfo}><img src={japan} alt="" className={styles.imgs} />
+                <div className={styles.mainBlocksInfo}><img src={warszawa} alt="" className={styles.imgs} />
                     <div href='' className={styles.mainBlocksInfoTitle}>
-                        <Link to='login'>Japan</Link>
+                        <Link to='login'>Warsaw</Link>
+                    </div>
+                    <div className={styles.mainBlocksInfoDescription}>  столиця Польщі з 1596 року, порт на річці Вісла, адміністративний центр Мазовецького воєводства. Місто є місцем розташування центральних органів влади Республіки Польща
+                        <Link to='madrid' className={styles.mainBlocksInfoButton}>Перейти</Link>
+                    </div>
+                </div>
+                <div className={styles.mainBlocksInfo}><img src={tokyo} alt="" className={styles.imgs} />
+                    <div href='' className={styles.mainBlocksInfoTitle}>
+                        <Link to='login'>Tokyo</Link>
+                    </div>
+                    <div className={styles.mainBlocksInfoDescription}>  столиця Японської Держави, столична префектура в Японії, в регіоні Канто. Розташована в східній частині острова Хонсю, на узбережжі Токійської затоки Тихого океану.
+                        <Link to='madrid' className={styles.mainBlocksInfoButton}>Перейти</Link>
                     </div>
                 </div>
 
